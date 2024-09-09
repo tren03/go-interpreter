01d20ff92c239d1c87a4f16540474e747dfca9e5
vishnu <vishnusethuraman@gmail.com>
2024-09-09 18:25:06 +0530

    lexer test works on keywords, numbers, ignores spaces and identifies identifiers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -9,12 +9,14 @@ type Lexer struct {
 	ch          byte // current char under examination
 }
 
+// Initialize all fields of the lexer
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
-	l.readChar() // Initialize all fields of the lexer
+	l.readChar() 
 	return l
 }
 
+// Read next char of the input, and Handle EOF
 func (l *Lexer) readChar() {
 	if l.readPostion >= len(l.input) {
 		l.ch = 0 // ASCII code for NIL
@@ -25,8 +27,11 @@ func (l *Lexer) readChar() {
 	l.readPostion += 1
 }
 
+// Returns the token struct of the parsed char
 func (l *Lexer) NextToken() token.Token {
+
 	var tok token.Token
+	l.skipWhiteSpace()
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -47,14 +52,66 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isletter(l.ch){
+			tok.Literal = l.readIdentifier() // read the entire string
+			tok.Type = token.LookupIdent(tok.Literal) // returns IDENT if its a literal, else the type of keyword
+			return tok // return it here instead of after the switch, since the above two move the readpostition and postition, we do not need to call l.readChar() after switch
+		}else if isDigit(l.ch){
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+			}else{
+			tok = newToken(token.ILLEGAL,l.ch)
+		}
 	}
 	l.readChar()
 	return tok
 }
 
+// Used to read the full content of a string, if we encounter a letter, to determine the full lenght of the identifier
+func(l *Lexer)readIdentifier()string {
+	position := l.position
+	for isletter(l.ch){
+		l.readChar()
+	}
+	return l.input[position:l.position] // Slice the input the only return the string
+
+}
+
+// Defined all the types of char allowed in our identifiers, or keywords
+func isletter(ch byte) bool{
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_' // Since _ can also be a part of the string -> which may be a identifier/keyword
+}
+
+// Creates a new token struct, given the type and char
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{
 		Type:    tokenType,
 		Literal: string(ch),
 	}
 }
+
+// Skips over the whitespaces of the input
+func(l *Lexer)skipWhiteSpace(){
+	for l.ch == ' '||l.ch=='\t'||l.ch=='\n'||l.ch=='\r'{
+		l.readChar()
+	}
+
+}
+
+// Checks if current char the lexer points to it a int
+func isDigit(ch byte) bool{
+	return ch >= '0' && ch <='9'
+}
+
+// If the number is multidigit, it needs to be read 
+func(l *Lexer)readNumber() string {
+	position := l.position
+	for isDigit(l.ch){
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
--- token/token.go
+++ token/token.go
@@ -14,7 +14,7 @@ const(
 	EOF = "EOF"
 
 	// Identifier, Literal
-	IDNET = "IDENT"
+	IDENT = "IDENT"
 	INT = "INT"
 
 	// Operators
@@ -36,3 +36,19 @@ const(
 	LET = "LET"
 
 )
+
+// Defines the correct Token type for keywords
+var keywords = map[string]TokenType{
+	"fn":FUNCTION,
+	"let":LET,
+}
+
+// Checks if string detected is a keyword and returns the type of token
+func LookupIdent(ident string) TokenType{
+	if tok,ok := keywords[ident];ok{ // The ok variable holds bool value which signinfies if element is there in map or not
+		return tok // return the value of the key
+	}
+	return IDENT // if it doesnt exists in keyword map, then it is of IDENT type
+
+
+}
